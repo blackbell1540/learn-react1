3cf21c53786c0db657e667f5eabe32f1219f81de
jiyoon <shy817@naver.com>
2021-08-30 17:10:41 +0900

    config ignore

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { User } = require('./models/User');
+const config = require('./config/key');
+
 const app = express();
 const port = 5000;
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jiyoon:wldbs817@learnreact.sbuwz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+// application/x-www-form-urlendcoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application /json
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+
+    // save는 몽고디비 함수
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        });
+    });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
