3f90f2a280b0f5eee96bb9048fb7e0b819e5e8ed
jiyoon <shy817@naver.com>
2021-10-05 17:32:56 +0900

    createStore의 다른 방법 테스트

--- client/src/index.js
+++ client/src/index.js
@@ -7,21 +7,30 @@ import 'antd/dist/antd.css';
 
 // redux
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore} from 'redux';
+import { applyMiddleware, compose, createStore} from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers';
 
 // store는 객체만 받을 수 있지만 미들웨어를 통해서 promise와 function도 받을 수 있게 한다.
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+// const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
+const store = createStore(
+  Reducer, 
+  compose(
+    applyMiddleware(promiseMiddleware, ReduxThunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+); 
 
 ReactDOM.render(
   <Provider
-    store={createStoreWithMiddleware(Reducer,
-      // extensions...
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}
+    // store={createStoreWithMiddleware(Reducer,
+    //   // extensions...
+    //   window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //   window.__REDUX_DEVTOOLS_EXTENSION__()
+    // )}
+    store={store}
   >
     <App />
   </Provider>,
